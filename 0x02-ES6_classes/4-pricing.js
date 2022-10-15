b1953757198404a6547b777bfac71b461bc7d53e
lhet <lmagnan17@alustudent.com>
2022-10-15 01:37:03 +0000

    classe files added

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+export default class Pricing {
+    constructor(_amount, _currency) {
+        this._amount = _amount;
+        this._currency = _currency;
+    }
+
+    set amount(myAmount) {
+        this._amount = myAmount;
+    }
+
+    set currency(myCurrency) {
+        if (!(newCurrency instanceof Currency)) throw TypeError('currency must be an instance of Currency');
+        this._currency = myCurrency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    displayFullPrice() {
+        return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return `${amount} * ${conversionRate}`;
+    }
+}
